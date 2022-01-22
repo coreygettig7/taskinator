901a13a19084047f33036a7e1587848b5686a85b
Corey Gettig <coreygettig7@gmail.com>
2022-01-22 14:01:49 -0500

    forgot to call load task funtion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
+var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
-var taskIdCounter = 0;
 var pageContentEl = document.querySelector("#page-content");
 var tasksInProgressEl = document.querySelector("#tasks-in-progress");
 var tasksCompletedEl = document.querySelector("#tasks-completed");
@@ -237,3 +237,5 @@ var loadTasks = function() {
 };
 
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
+
+loadTasks();
